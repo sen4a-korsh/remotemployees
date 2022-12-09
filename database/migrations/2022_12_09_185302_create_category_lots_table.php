b7155275eb860085e447d891f51d3e0fb0af8743
Semen <skorshenkos@gmail.com>
2022-12-09 21:29:10 +0200

    add migrations, models, seeders, controllers

---
+++ database/migrations/2022_12_09_185302_create_category_lots_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_lots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lot_id')
+                ->constrained('lots')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_lots');
+    }
+};
