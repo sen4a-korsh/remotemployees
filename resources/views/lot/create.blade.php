9c1975bf7fae7c1d3daafbdc94c01fa3aa83ff5d
Semen <skorshenkos@gmail.com>
2022-12-13 23:12:06 +0200

    add errors for create and edit lots

--- resources/views/lot/create.blade.php
+++ resources/views/lot/create.blade.php
@@ -11,14 +11,23 @@
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" name="title" class="form-control" id="title" placeholder="Title" value="{{ old('title') }}">
+            @error('title')
+            <p class="text-danger">Fill in the field "Title"</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <textarea type="text" name="description" class="form-control" id="description" placeholder="Description">{{ old('description') }}</textarea>
+            @error('description')
+            <p class="text-danger">Fill in the field "Description"</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
             <input type="number" name="price" class="form-control" id="price" placeholder="Price" value="{{ old('price') }}">
+            @error('price')
+            <p class="text-danger">Fill in the field "Price"</p>
+            @enderror
         </div>
         <div class="mb-3">
             <label for="categories" class="form-label">Category</label>
